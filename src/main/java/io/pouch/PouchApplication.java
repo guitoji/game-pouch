8a882a2f357d2e928b25aaf732287f645e9008c7
guitoji <guilherme.limaal22@outlook.com>
2026-09-05 10:33:12 -0300

    Initial commit

---
+++ src/main/java/io/pouch/PouchApplication.java
@@ -0,0 +1,13 @@
+package io.pouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PouchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pouch/PouchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
