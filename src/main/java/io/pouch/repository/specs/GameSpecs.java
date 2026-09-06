3184976d9dee6d9c4f3a4a68d30886b869d43156
guitoji <guilherme.limaal22@outlook.com>
2026-09-06 18:23:10 -0300

    Added a comment at GameSpecs

--- src/main/java/io/pouch/repository/specs/GameSpecs.java
+++ src/main/java/io/pouch/repository/specs/GameSpecs.java
@@ -6,6 +6,12 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class GameSpecs {
 
+    /*
+        I also wanted to create a specification for Categories that would serve as a filter for game searches.
+        However, I ran into a problem due to how the categories are stored in the database.
+        Our entity has a list of categories stored as `varchar[]`.
+     */
+
     public static Specification<Game> titleLike(String title) {
         return ((root, query, cb) -> cb.like(cb.upper(root.get("title")), "%" + title.toUpperCase() + "%"));
     }
