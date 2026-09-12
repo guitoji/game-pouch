8dc99937a7c8624bb49e0d0ab68697894a9be777
guitoji <guilherme.limaal22@outlook.com>
2026-09-12 01:36:51 -0300

    UserService created

---
+++ src/main/java/io/pouch/service/UserService.java
@@ -0,0 +1,17 @@
+package io.pouch.service;
+
+import io.pouch.repository.RoleRepository;
+import io.pouch.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+}
