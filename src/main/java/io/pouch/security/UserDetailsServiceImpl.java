a22b82428c9a3d29626b24db544ff51a385bfe45
guitoji <guilherme.limaal22@outlook.com>
2026-09-12 00:22:24 -0300

    Implementation of UserDetailsService created

---
+++ src/main/java/io/pouch/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package io.pouch.security;
+
+import io.pouch.exceptions.UserNotFoundException;
+import io.pouch.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(() -> new UserNotFoundException("User not found in the data base"));
+    }
+}
