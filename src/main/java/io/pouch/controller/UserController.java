249ff7a009d096b7a08edd1a58ecb9440a6b326f
guitoji <guilherme.limaal22@outlook.com>
2026-09-12 01:37:54 -0300

    UserController created

---
+++ src/main/java/io/pouch/controller/UserController.java
@@ -0,0 +1,16 @@
+package io.pouch.controller;
+
+import io.pouch.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
