794d5fcabc76edbee59332879e28c8ecf0d219ab
guitoji <guilherme.limaal22@outlook.com>
2026-09-12 01:35:01 -0300

    RoleRepository created

---
+++ src/main/java/io/pouch/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package io.pouch.repository;
+
+import io.pouch.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
